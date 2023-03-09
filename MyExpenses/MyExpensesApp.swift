b59087e98e59f40b55b954825d802fd9f216b75c
Fly <kiwipromm@gmail.com>
2023-03-09 09:48:03 +0100

    Initial Commit

---
+++ MyExpenses/MyExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyExpensesApp.swift
+//  MyExpenses
+//
+//  Created by KADIR ISMAILI on 9.3.23.
+//
+
+import SwiftUI
+
+@main
+struct MyExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
